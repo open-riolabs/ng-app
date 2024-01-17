0d59f1984e5628dba9f168a64e4ae065c8e388c3
Giuseppe Riolo <riolo.giuseppe@gmail.com>
2024-01-17 15:20:13 +0100

    added interceptor

---
+++ projects/rlb/ng-app/src/lib/auth/token-oauth-interceptor.ts
@@ -0,0 +1,32 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { Inject, Injectable } from "@angular/core";
+import { Observable, switchMap, map } from 'rxjs';
+import { AuthenticationService } from "./services/auth.service";
+import { IConfiguration, RLB_CFG } from "../configuration";
+
+@Injectable()
+export class TokenOauthInterceptor implements HttpInterceptor {
+
+  constructor(
+    private authenticationService: AuthenticationService,
+    @Inject(RLB_CFG) private appconfig: IConfiguration
+  ) { }
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const endpoints = this.appconfig.endpoints ? Object.values(this.appconfig.endpoints) : []
+    endpoints.some(e => request.url.includes(e))
+    if (!endpoints.some(e => request.url.includes(e))) {
+      return next.handle(request);
+    }
+    return this.authenticationService.accessToken$.pipe(
+      switchMap(token => {
+        if (token) {
+          const newRequest = request.clone({
+            setHeaders: { Authorization: `Bearer ${token}` }
+          });
+          return next.handle(newRequest);
+        }
+        return next.handle(request);
+      })
+    );
+  }
+}
--- projects/rlb/ng-app/src/public-api.ts
+++ projects/rlb/ng-app/src/public-api.ts
@@ -1,5 +1,5 @@
 import { EnvironmentProviders, Provider, importProvidersFrom } from '@angular/core'
-import { ProjectConfiguration, RLB_CFG, RLB_CFG_CMS, RLB_CFG_ENV, RLB_CFG_I18N, RLB_CFG_PAGES } from './lib/configuration'
+import { ProjectConfiguration, RLB_CFG, RLB_CFG_AUTH, RLB_CFG_CMS, RLB_CFG_ENV, RLB_CFG_I18N, RLB_CFG_PAGES } from './lib/configuration'
 import { RlbAppModule } from './lib/rlb-app.module'
 import { RlbBootstrapModule } from '@rlb/ng-bootstrap'
 import { ModalAppsComponent } from './lib/modals/modal-apps.component'
@@ -18,16 +18,11 @@ import { AppContextEffects } from './lib/store/app-context/app-context.effects'
 export * from './lib/services'
 export * from './lib/pipes'
 export * from './lib/templates'
-export * from './lib/auth/guards/oauth-password.guard'
-export * from './lib/auth/guards/oauth.guard'
-export * from './lib/auth/services/oauth-password.service'
-export * from './lib/auth/user-claims'
-export * from './lib/auth/user-info'
+export * from './lib/auth'
 export * from './lib/rlb-app.module'
 
 export * from './lib/pages/shared.routes'
 export * from './lib/configuration'
-export * from './lib/auth/auth.provider'
 
 export * from './lib/store/auth/auth.actions'
 export * from './lib/store/auth/auth.model'
@@ -55,7 +50,7 @@ export function provideRlbConfig<T = { [k: string]: any }>(env: ProjectConfigura
     { provide: RLB_CFG_ENV, useValue: env.environment },
     { provide: RLB_CFG_CMS, useValue: env.cms },
     { provide: RLB_CFG_PAGES, useValue: env.pages },
-    { provide: RLB_CFG_I18N, useValue: env.i18n }
+    { provide: RLB_CFG_I18N, useValue: env.i18n },
   ]
 }
 
