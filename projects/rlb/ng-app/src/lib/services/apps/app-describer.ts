e86736e91c4e95c7f50aef90956b8cb993276333
Giuseppe Riolo <riolo.giuseppe@gmail.com>
2025-05-14 23:22:03 +0200

    Enhance AppDescriber interface to support generic data type

--- projects/rlb/ng-app/src/lib/services/apps/app-describer.ts
+++ projects/rlb/ng-app/src/lib/services/apps/app-describer.ts
@@ -2,8 +2,9 @@ import { Routes } from "@angular/router";
 import { AppInfo } from "./app";
 import { EnvironmentProviders, Provider } from "@angular/core";
 
-export interface AppDescriber {
+export interface AppDescriber<T = any> {
   info: AppInfo;
   routes?: Routes
   providers?: (Provider | EnvironmentProviders)[]
+  data?: T
 }
