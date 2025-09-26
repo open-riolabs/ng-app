2242bef045e521f0a2707b660e71024dec56ab9e
Denis <neorimne@gmail.com>
2025-09-26 15:33:36 +0200

    feat: (RlbLoggerService) enhance logger with additional info, add class prefix support, timestamp

--- projects/rlb/ng-app/src/lib/auth/services/rlb-logger.service.ts
+++ projects/rlb/ng-app/src/lib/auth/services/rlb-logger.service.ts
@@ -3,6 +3,14 @@ import { AbstractLoggerService } from 'angular-auth-oidc-client';
 
 export type LogLevel = 'off' | 'error' | 'warn' | 'info' | 'debug' | 'all';
 
+export type LoggerContext = {
+	error: (...args: any[]) => void;
+	warn: (...args: any[]) => void;
+	info: (...args: any[]) => void;
+	debug: (...args: any[]) => void;
+	log: (...args: any[]) => void;
+};
+
 const LEVEL_PRIORITIES: Record<LogLevel, number> = {
 	off: 0,
 	error: 1,
@@ -13,13 +21,13 @@ const LEVEL_PRIORITIES: Record<LogLevel, number> = {
 };
 
 @Injectable({ providedIn: 'root' })
-export class RlbLoggerService implements AbstractLoggerService {
-	private currentLevel: LogLevel = 'warn';
+export class RlbLoggerService extends AbstractLoggerService {
+	private currentLevel: LogLevel = 'all';
 	private timestamps = true;
 	
 	setLogLevel(level: LogLevel) {
-		if (!Object.prototype.hasOwnProperty.call(LEVEL_PRIORITIES, level)) {
-			throw new Error(`Unknown log level: ${String(level)}`);
+		if (!(level in LEVEL_PRIORITIES)) {
+			throw new Error(`Unknown log level: ${level}`);
 		}
 		this.currentLevel = level;
 	}
@@ -32,41 +40,60 @@ export class RlbLoggerService implements AbstractLoggerService {
 		this.timestamps = enabled;
 	}
 	
-	logError(message: any, ...args: any[]): void {
-		if (this.shouldLog('error')) {
-			console.error(this.prefix('error'), message, ...args);
+	/**
+	 * returns logger with context / class name
+	 */
+	for(context: string): LoggerContext {
+		return {
+			error: (...args: any[]) => this.logError(context, ...args),
+			warn: (...args: any[]) => this.logWarning(context, ...args),
+			info: (...args: any[]) => this.logInfo(context, ...args),
+			debug: (...args: any[]) => this.logDebug(context, ...args),
+			log: (...args: any[]) => this.log(context, ...args),
+		};
+	}
+	
+	logError(contextOrMessage: any, ...args: any[]): void {
+		this.print('error', contextOrMessage, args);
+	}
+	logWarning(contextOrMessage: any, ...args: any[]): void {
+		this.print('warn', contextOrMessage, args);
+	}
+	logDebug(contextOrMessage: any, ...args: any[]): void {
+		this.print('debug', contextOrMessage, args);
+	}
+	logInfo(contextOrMessage: any, ...args: any[]): void {
+		this.print('info', contextOrMessage, args);
+	}
+	log(contextOrMessage: any, ...args: any[]): void {
+		this.print('all', contextOrMessage, args);
+	}
+	
+	private print(level: LogLevel, contextOrMessage: any, args: any[]) {
+		if ((LEVEL_PRIORITIES[level] ?? 0) > (LEVEL_PRIORITIES[this.currentLevel] ?? 0)) return;
+		
+		let context = 'GLOBAL';
+		let messageArgs: any[] = [];
+		
+		if (args.length > 0) {
+			context = contextOrMessage;
+			messageArgs = args;
+		} else {
+			messageArgs = [contextOrMessage];
 		}
-	}
-	
-	logWarning(message: any, ...args: any[]): void {
-		if (this.shouldLog('warn')) {
-			console.warn(this.prefix('warn'), message, ...args);
+		
+		const timePrefix = this.timestamps ? `[${new Date().toISOString()}]` : '';
+		const prefix = `[${level.toUpperCase()}][${context}]${timePrefix} -`;
+		
+		switch (level) {
+			case 'error':
+				console.error(`%c${prefix}`, 'color:red;', ...messageArgs);
+				break;
+			case 'warn':
+				console.warn(`%c${prefix}`, 'color:orange;', ...messageArgs);
+				break;
+			default:
+				console.log(prefix, ...messageArgs);
 		}
-	}
-	
-	logDebug(message: any, ...args: any[]): void {
-		if (this.shouldLog('debug')) {
-			console.debug(this.prefix('debug'), message, ...args);
-		}
-	}
-	
-	logInfo(message: any, ...args: any[]): void {
-		if (this.shouldLog('info')) {
-			console.info(this.prefix('info'), message, ...args);
-		}
-	}
-	
-	log(message: any, ...args: any[]): void {
-		if (this.shouldLog('info')) {
-			console.log(this.prefix('log'), message, ...args);
-		}
-	}
-	
-	private prefix(level: string) {
-		return this.timestamps ? `[${level.toUpperCase()}] ${new Date().toISOString()} -` : `[${level.toUpperCase()}]`;
-	}
-	
-	private shouldLog(messageLevel: LogLevel): boolean {
-		return (LEVEL_PRIORITIES[messageLevel] ?? 0) <= (LEVEL_PRIORITIES[this.currentLevel] ?? 0);
 	}
 }
