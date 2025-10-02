81635aaa0a73087552aac90ee8c1d1bebf507206
Denis <neorimne@gmail.com>
2025-10-02 09:44:10 +0200

    revert app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,40 +1,16 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { TestComponent } from "@/home/test.component";
-import { Test2Component } from "@/home/test2.component";
-import { Test3Component } from "@/home/test3.component";
 
 export const routes: Routes = [{
-  path: '',
-  data: { template: 'app', breadcrumb: 'Home' },
-  component: HomeComponent,
-	children: [
-		{
-			path: 'dashboard',
-			component: TestComponent,
-			data: { breadcrumb: 'Dashboard' },
-			children: [
-				{
-					path: 'booking',
-					component: Test2Component,
-					data: { breadcrumb: 'Booking' },
-					children: [
-						{
-							path: 'booking-info',
-							component: Test3Component,
-							data: { breadcrumb: 'Booking Info' },
-						}
-					]
-				},
-			],
-		},
-	],
+	path: '',
+	data: { template: 'app' },
+	component: HomeComponent
 },
-{
-  path: 'chatto',
-  component: HomeComponent
-},
-{
-  path: 'settings/chatto',
-  component: HomeComponent
-}];
+	{
+		path: 'chatto',
+		component: HomeComponent
+	},
+	{
+		path: 'settings/chatto',
+		component: HomeComponent
+	}];
