9eb7edaaff694f3c6893744a6912ee6c243ef6cb
Denis <neorimne@gmail.com>
2025-10-14 15:16:47 +0200

    fix: (oauthGuard) add app config auth check

--- projects/rlb/ng-app/src/lib/auth/guards/oauth.guard.ts
+++ projects/rlb/ng-app/src/lib/auth/guards/oauth.guard.ts
@@ -1,19 +1,26 @@
 import { inject } from '@angular/core';
-import { CanActivateFn } from '@angular/router';
-import { map, take } from 'rxjs';
+import { CanActivateFn, Router } from '@angular/router';
+import { map, take, withLatestFrom } from 'rxjs';
 import { AuthenticationService } from "../services/auth.service";
+import { Store } from "@ngrx/store";
+import { authsFeatureKey, BaseState } from "../../store";
 
 export const oauthGuard: CanActivateFn = (route, state) => {
-  const authService = inject(AuthenticationService);
-
-  return authService.isAuthenticated$.pipe(
-    take(1),
-    map((isAuthenticated) => {
-      if (!isAuthenticated) {
-				authService.authorize()
-        return false;
-      }
-      return true;
-    }),
-  );
+	const authService = inject(AuthenticationService);
+	const store = inject(Store<BaseState>);
+	const router = inject(Router);
+	
+	return authService.isAuthenticated$.pipe(
+		take(1),
+		withLatestFrom(store.select(s => s[authsFeatureKey].isAuth)),
+		map(([isAuthenticated, authEnabled]) => {
+			if (!isAuthenticated && authEnabled) {
+				authService.authorize();
+				// TODO: Consider possible alternative:
+				// return router.createUrlTree(['/login']);
+				return false;
+			}
+			return true;
+		}),
+	);
 };
