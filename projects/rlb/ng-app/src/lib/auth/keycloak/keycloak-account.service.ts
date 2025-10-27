ac447cb67f64d5ff4171a23a4044ab37238778a4
Giuseppe Riolo <riolo.giuseppe@gmail.com>
2025-10-27 10:13:52 +0100

    .

--- projects/rlb/ng-app/src/lib/auth/keycloak/keycloak-account.service.ts
+++ projects/rlb/ng-app/src/lib/auth/keycloak/keycloak-account.service.ts
@@ -1,73 +1,61 @@
 import { HttpClient } from "@angular/common/http";
-import { Inject, Injectable, Optional } from "@angular/core";
-import { Store } from "@ngrx/store";
+import { Injectable } from "@angular/core";
 import { OidcSecurityService } from "angular-auth-oidc-client";
-import { EMPTY, map, Observable } from "rxjs";
-import { AuthConfiguration, RLB_CFG_AUTH } from "../../configuration";
+import {  filter, map, Observable, switchMap } from "rxjs";
 import { ErrorManagementService } from "../../services/errors/error-management.service";
-import { authsFeatureKey, BaseState } from "../../store";
 import { KeycloakCredential, KeycloakDevice, KeycloakSession, KeycloakUser } from "./";
+import { AuthenticationService } from "..";
 
 @Injectable({
   providedIn: 'root',
 })
 export class KeycloakProfileService {
 
-  get currentProvider() {
-    const currentProvider = this.store.selectSignal((state) => state[authsFeatureKey].currentProvider)();
-    return this.authOptions?.providers.find((provider) => provider.configId === currentProvider);
-  }
-
-  private get baseUrl() {
-    return `${this.currentProvider?.authority}/account`;
-  }
-
   constructor(
-    @Optional() @Inject(RLB_CFG_AUTH) @Optional() private authOptions: AuthConfiguration,
     private http: HttpClient,
-    private readonly store: Store<BaseState>,
+    private readonly authService: AuthenticationService,
     private readonly errorManagementService: ErrorManagementService,
     private readonly oidcSecurityService: OidcSecurityService) { }
 
+  private get baseUrl() {
+    return `${this.authService.currentProvider?.authority}/account`;
+  }
+
   getUserProfile(): Observable<KeycloakUser> {
-    if (!this.store.selectSignal((state) => state[authsFeatureKey].isAuth)()) {
-      return EMPTY;
-    }
-    const token = this.store.selectSignal((state) => state[authsFeatureKey].accessToken)();
-    const url = `${this.baseUrl}`;
-    return this.http.get<any>(url, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    })
-      .pipe(this.errorManagementService.manageUI('error', 'dialog'));
+    return this.authService.isAuthenticated$.pipe(
+      filter(isAuth => isAuth),
+      switchMap(() => this.authService.accessToken$),
+      switchMap(token => this.http.get<any>(this.baseUrl, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })),
+      this.errorManagementService.manageUI('error', 'dialog')
+    )
   }
 
   updateUserProfile(data: KeycloakUser): Observable<void> {
-    if (!this.store.selectSignal((state) => state[authsFeatureKey].isAuth)()) {
-      return EMPTY;
-    }
-    const token = this.store.selectSignal((state) => state[authsFeatureKey].accessToken)();
-    const url = `${this.baseUrl}`;
-    return this.http.post<any>(url, data, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    })
-      .pipe(this.errorManagementService.manageUI('error', 'dialog'));
+    return this.authService.isAuthenticated$.pipe(
+      filter(isAuth => isAuth),
+      switchMap(() => this.authService.accessToken$),
+      switchMap(token => this.http.post<void>(this.baseUrl, data, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })),
+      this.errorManagementService.manageUI('error', 'dialog')
+    )
   }
 
   getDevices(): Observable<KeycloakSession[]> {
-    if (!this.store.selectSignal((state) => state[authsFeatureKey].isAuth)()) {
-      return EMPTY;
-    }
-    const token = this.store.selectSignal((state) => state[authsFeatureKey].accessToken)();
-    const url = `${this.baseUrl}/sessions/devices`;
-    return this.http.get<KeycloakDevice[]>(url, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    }).pipe(
+    return this.authService.isAuthenticated$.pipe(
+      filter(isAuth => isAuth),
+      switchMap(() => this.authService.accessToken$),
+      switchMap(token => this.http.get<KeycloakDevice[]>(`${this.baseUrl}/sessions/devices`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })),
       map((devices) => devices.map((device) => device.sessions.map((session) => {
         session.os = device.os;
         session.osVersion = device.osVersion;
@@ -76,39 +64,40 @@ export class KeycloakProfileService {
         session.clientslist = session.clients.map((client) => client.clientName).join(', ');
         return session;
       })).flat()),
-      this.errorManagementService.manageUI('error', 'dialog'));
+      this.errorManagementService.manageUI('error', 'dialog')
+    )
   }
 
   getCredentials(): Observable<KeycloakCredential[]> {
-    if (!this.store.selectSignal((state) => state[authsFeatureKey].isAuth)()) {
-      return EMPTY;
-    }
-    const token = this.store.selectSignal((state) => state[authsFeatureKey].accessToken)();
-    const url = `${this.baseUrl}/credentials`;
-    return this.http.get<any>(url, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    })
-      .pipe(this.errorManagementService.manageUI('error', 'dialog'));
+    return this.authService.isAuthenticated$.pipe(
+      filter(isAuth => isAuth),
+      switchMap(() => this.authService.accessToken$),
+      switchMap(token => this.http.get<KeycloakCredential[]>(`${this.baseUrl}/credentials`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })),
+      this.errorManagementService.manageUI('error', 'dialog')
+    );
   }
 
   removeCredential(id: string): Observable<void> {
-    if (!this.store.selectSignal((state) => state[authsFeatureKey].isAuth)()) {
-      return EMPTY;
-    }
-    const token = this.store.selectSignal((state) => state[authsFeatureKey].accessToken)();
-    const url = `${this.baseUrl}/credentials/${id}`;
-    return this.http.delete<void>(url, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    })
-      .pipe(this.errorManagementService.manageUI('error', 'dialog'));
+    return this.authService.isAuthenticated$.pipe(
+      filter(isAuth => isAuth),
+      switchMap(() => this.authService.accessToken$),
+      switchMap(token => {
+        return this.http.delete<void>(`${this.baseUrl}/credentials/${id}`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+      }),
+      this.errorManagementService.manageUI('error', 'dialog')
+    );
   }
 
   configureOTP() {
-    return this.oidcSecurityService.authorize(this.currentProvider?.configId, {
+    return this.oidcSecurityService.authorize(this.authService.currentProvider?.configId, {
       customParams: {
         kc_action: "CONFIGURE_TOTP"
       }
@@ -116,7 +105,7 @@ export class KeycloakProfileService {
   }
 
   updatePassword() {
-    return this.oidcSecurityService.authorize(this.currentProvider?.configId, {
+    return this.oidcSecurityService.authorize(this.authService.currentProvider?.configId, {
       customParams: {
         kc_action: "UPDATE_PASSWORD"
       }
