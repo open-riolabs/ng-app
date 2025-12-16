2e576e8595e53dd0222d5fc5752c0c28db8a6448
Denis <neorimne@gmail.com>
2025-12-16 12:14:43 +0100

    code cleanup

--- projects/rlb/ng-app/src/lib/auth/auth-init.ts
+++ projects/rlb/ng-app/src/lib/auth/auth-init.ts
@@ -1,7 +1,5 @@
-import { APP_INITIALIZER } from '@angular/core';
 import { AuthenticationService } from "./services/auth.service";
 
 export function authInitializer(authService: AuthenticationService) {
-  console.warn("authInitializer!!")
   return () => authService.checkAuthMultiple();
 }
