cf65429c33f79179dc00cd6d6143f224a31883d5
Denis <neorimne@gmail.com>
2026-02-19 10:30:55 +0100

    chore: update deps feat: (Acl) enhance AppInitAclProvider with check for existing app with same id to prevent double finalize

--- src/app/app-init-acl.provider.ts
+++ src/app/app-init-acl.provider.ts
@@ -1,6 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { AclConfiguration, AppContextActions, BaseState, RlbInitProvider, UserResource } from '@open-rlb/ng-app';
+import {
+  AclConfiguration,
+  AppContextActions,
+  appContextFeatureKey,
+  BaseState,
+  RlbInitProvider,
+  UserResource
+} from '@open-rlb/ng-app';
 
 /*
   Example how to finalizeApp with ACL enabled
@@ -13,20 +20,27 @@ export class AppInitAclProvider implements RlbInitProvider {
       return;
     }
 
+    const currentApps = store.selectSignal(state => state[appContextFeatureKey].apps)()
+
     resources.forEach(company => {
       company.resources.forEach(res => {
-        const appData = {
-          title: res.friendlyName,
-          appName: res.friendlyName,
-          [acl.businessIdKey]: company.resourceBusinessId,
-          [acl.resourceIdKey]: res.resourceId,
-        };
+        const appId = `chat-${res.resourceId}`
+        if (currentApps.findIndex((app) => app.id === appId) === -1) {
+          const appData = {
+            title: res.friendlyName,
+            appName: res.friendlyName,
+            [acl.businessIdKey]: company.resourceBusinessId,
+            [acl.resourceIdKey]: res.resourceId,
+          };
 
-        store.dispatch(AppContextActions.finalizeApp({
-          appType: 'chat',
-          appId: `chat-${res.resourceId}`,
-          data: appData,
-        }));
+          store.dispatch(AppContextActions.finalizeApp({
+            appType: 'chat',
+            appId,
+            data: appData,
+          }));
+        } else {
+          console.warn(`[AppInitAclProvider]: appId ${appId} exists already, skip finalize`)
+        }
       });
     });
   }
